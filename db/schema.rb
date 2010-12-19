93b284150db796bcc160cedf1750195f8b33c403
maukoquiroga <mauko@pictorical.com>
2010-12-19 16:37:23 -0300

    Migration to Drop Useless Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101219182401) do
+ActiveRecord::Schema.define(:version => 20101219193434) do
 
   create_table "artists", :force => true do |t|
     t.string   "first_name"
