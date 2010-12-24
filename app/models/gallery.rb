329e7ec076e9201a5a7b67e740b037b9b41a91c6
maukoquiroga <mauko@pictorical.com>
2010-12-24 02:13:57 -0300

    Chan

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,6 +1,6 @@
 class Gallery < ActiveRecord::Base
 
-  acts_as_indexed :fields => :name
+  acts_as_indexed :fields => [:name, :body]
 
   validates_presence_of :name
   validates_uniqueness_of :name
