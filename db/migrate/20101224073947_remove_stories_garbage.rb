d59d31b4b3d51ff4309a811f2a717671afabfedf
maukoquiroga <mauko@pictorical.com>
2010-12-24 05:28:41 -0300

    _covers ready

--- db/migrate/20101224073947_remove_stories_garbage.rb
+++ db/migrate/20101224073947_remove_stories_garbage.rb
@@ -1,9 +1,9 @@
 class RemoveStoriesGarbage < ActiveRecord::Migration
   def self.up
-    UserPlugin.where(:name => :stories).each { |t| t.destroy! }
-    Slug.where(:name => "stories-page").each { |t| t.destroy! }
+    UserPlugin.where(:name => :stories).each { |t| t.destroy }
+    Slug.where(:name => "stories-page").each { |t| t.destroy }
     Page.where(:title => "Stories").each { |t| t.destroy! }
-    PagePart.where(:page_id => 9).each { |t| t.destroy! }
+    PagePart.where(:page_id => 9).each { |t| t.destroy }
   end
 
   def self.down
