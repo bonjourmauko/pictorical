8ef7e8582bf03d5225e67a9dd61ddbfd2169bf5a
victor <victor@pictorical.com>
2011-03-20 20:04:42 -0700

    now we have static pages controller & legal stuff

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  
+  def terms_and_conditions
+  end
+
+  def privacy_policy
+  end
+
+  def website_disclaimer
+  end
+
+  def copyright_notice
+  end
+
+  def legal
+  end
+  
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Refinery::Application.routes.draw do
   
   root :to => 'books#home'
-    
+  
+  match "/terms-and-conditions", :to => "pages#terms_and_conditions", :as => :terms_and_conditions, :via => :get
+  match "/privacy-policy", :to => "pages#privacy_policy", :as => :privacy_policy, :via => :get
+  match "/website-disclaimer", :to => "pages#website_disclaimer", :as => :website_disclaimer, :via => :get
+  match "/copyright-notice", :to => "pages#copyright_notice", :as => :copyright_notice, :via => :get
+  match "/legal", :to => "pages#legal", :as => :legal, :via => :get
+  
   # REFINERY CMS ================================================================
 
   filter(:refinery_locales) if defined?(RoutingFilter::RefineryLocales) # optionally use i18n.
