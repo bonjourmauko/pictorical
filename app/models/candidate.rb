39202a4821dad45d34a5745ef7f6ec9f469b9085
maukoquiroga <mauko@pictorical.com>
2011-03-27 21:22:07 -0400

    country select

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,2 +1,8 @@
 class Candidate < ActiveRecord::Base
+  attr_accessible   :name,
+                    :country,
+                    :email,
+                    :portfolio
+                    
+  attr_protected    :token
 end
