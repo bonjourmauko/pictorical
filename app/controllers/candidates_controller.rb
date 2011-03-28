e55dbefb93e390dd26161c093d97437a40ce13a3
maukoquiroga <mauko@pictorical.com>
2011-03-28 16:12:21 -0400

    solucion hacker para el routing

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,8 +1,13 @@
 class CandidatesController < ApplicationController
-  respond_to :html, :js
+  respond_to :html, :js, :json
   
   def new
-    @candidate = Candidate.new
+    @candidate =
+      if session[:candidate].nil?
+        Candidate.new
+      else
+        session[:candidate]
+      end
     session[:referral] = params[:referral] unless session[:referral]
     session[:http_referer] = request.env["HTTP_REFERER"] || 'none' unless session[:http_referer]
   end
@@ -11,8 +16,13 @@ class CandidatesController < ApplicationController
     @candidate = Candidate.new(params[:candidate])
     @candidate[:referral] = session[:referral]
     @candidate[:http_referer] = session[:http_referer]
-    @candidate.save
-    #redirect_to share_candidate_path @candidate[:token]
+    if @candidate.save
+      session[:candidate] = nil
+      redirect_to share_candidate_path @candidate[:token]
+    else
+      session[:candidate] = @candidate
+      redirect_to "/apply"
+    end
   end
   
   def share
